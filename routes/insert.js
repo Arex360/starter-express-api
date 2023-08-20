9aa32c37b89d581348b5914650877a8b02f781a2
Arex360 <122282190+arex520@users.noreply.github.com>
2023-08-20 23:43:11 +0500

    update

--- routes/insert.js
+++ routes/insert.js
@@ -1,13 +1,32 @@
-const express = require('express')
-const client = require('../backend/mongo')
-const router = express.Router()
-router.get('/insert/:key/:value',async (req,res)=>{
-    console.log("inserting")
-    const {key,value} = req.params
-    await client.connect()
-    const db = client.db("rfc")
-    const collection = await db.collection(key).insertOne({value})
-    await client.close()
-    res.send("sent")
-})
-module.exports = router
+const express = require('express');
+const client = require('../backend/mongo');
+const router = express.Router();
+
+router.post('/upsert/:key/:value', async (req, res) => {
+    console.log("upserting");
+    const { key, value } = req.params;
+    
+    try {
+        await client.connect();
+        const db = client.db("rfc");
+        
+        // Check if the document with the given key exists
+        const existingDoc = await db.collection(key).findOne({});
+
+        if (existingDoc) {
+            // Update the existing document
+            await db.collection(key).updateOne({}, { $set: { value } });
+        } else {
+            // Insert a new document
+            await db.collection(key).insertOne({ value });
+        }
+
+        await client.close();
+        res.send("sent");
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+module.exports = router;
