6e9a51ec87a63a4900a310ecba6792eae6c10df4
Arex360 <122282190+arex520@users.noreply.github.com>
2023-08-20 22:54:41 +0500

    Revert "udpate"
    This reverts commit d6efd4c4ac014025a9ebeda2395fc1f9547fce1c.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const cors = require('cors')
-const { root, insert, reterive } = require('./api/routes/routes')
 const app = express()
-
-app.use(cors())
-app.get('/',(req,res)=>{
-    res.send("hello")
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
 })
-app.listen(3001,()=>console.log('server started'))
+app.listen(process.env.PORT || 3000)
--- routes/routes.js
+++
@@ -1,4 +0,0 @@
-const root = require('./root')
-const insert = require('./insert')
-const reterive = require('./reterive')
-module.exports = {root,insert,reterive}
