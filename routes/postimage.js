44faeadbe4d76a70ba5aaca6e2e496abdf4c4d6f
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-12-04 02:21:18 +0500

    udpate

--- routes/postimage.js
+++ routes/postimage.js
@@ -1,27 +1,12 @@
-const express = require('express');
-const axios = require('axios');
-const router = express.Router();
-
-router.post('/post', async (req, res) => {
-    const { clientID, sourceImage, targetImage } = req.body;
-    const body = { clientID, sourceImage, targetImage };
-
-    try {
-        // Make a request to get the image data
-        const response = await axios.post('http://129.151.159.104:443/postPhoto', body);
-
-        // Download the image using the URL
-        const imageResponse = await axios.get(response.data, { responseType: 'arraybuffer' });
-
-        // Convert the image to base64
-        const base64Image = Buffer.from(imageResponse.data, 'binary').toString('base64');
-
-        // Send the base64 string as a response
-        res.send({ base64Image });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal Server Error' });
-    }
-});
-
-module.exports = router;
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+router.post('/post',(req,res)=>{
+    const {clientID,sourceImage,targetImage} = req.body
+    const body = {clientID,sourceImage,targetImage}
+    axios.post('http://129.151.159.104:443/postPhoto',body)
+    .then(r=>res.send(r.data))
+    .catch(e=>res.send(e))
+    //res.send('Welcome')
+})
+module.exports = router
