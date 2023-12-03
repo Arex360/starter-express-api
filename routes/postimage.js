6e34bfdb7e7e4ba20ead0209945c91e0b60b2d48
Arex360 <49303647+Arex360@users.noreply.github.com>
2023-12-04 02:08:31 +0500

    update

--- routes/postimage.js
+++ routes/postimage.js
@@ -2,7 +2,7 @@ const express = require('express')
 const axios = require('axios')
 const router = express.Router()
 router.get('/post',(req,res)=>{
-    const {clientID,sourceImage,targetImage} = req.params.body
+    const {clientID,sourceImage,targetImage} = req.body
     const body = {clientID,sourceImage,targetImage}
     axios.post('http://129.151.159.104:443/postPhoto',body).then(r=>res.send(r.data)).catch(e=>res.send(e))
     //res.send('Welcome')
